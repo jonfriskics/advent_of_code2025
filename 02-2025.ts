141bba2ef240118c9cb0fc68e17dc1f0522ff42f
Jon Friskics <jon-friskics@pluralsight.com>
2025-12-02 16:48:15 -0500

    cleanup testCase

--- 02-2025.ts
+++ 02-2025.ts
@@ -7,7 +7,6 @@ let testCase: string | undefined;
 // testCase = `
 //   11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
 // `;
-testCase = undefined;
 
 let puzzleLines = await readFile("puzzles/02-2025.txt", testCase);
 
